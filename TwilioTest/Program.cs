11f963699b31b12fe780c9ccad79abca61c2fa41
xvanaf005 <vana.frantisek7@seznam.cz>
2023-06-23 13:34:20 +0200

    SMS sender

--- TwilioTest/Program.cs
+++ TwilioTest/Program.cs
@@ -8,8 +8,8 @@ class SMSsender
     static void Main(string[] args)
     {
         //údaje z Twilio účtu
-        string accountSid = "AC50a7eb2f7966b4ab3c95928c7a953c99";
-        string authToken = "62a2bfc39aa6184974c9746e00335a53";
+        string accountSid = "";
+        string authToken = "";
 
         //odkaz na Twilio
         TwilioClient.Init(accountSid, authToken);
