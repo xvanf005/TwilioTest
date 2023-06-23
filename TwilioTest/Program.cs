84598573680b3e60a5498e1171c85fb0c83b132e
vanaf <vanaf@LAPTOP-06G4MT9I>
2023-06-23 13:17:54 +0200

    Přidejte soubory projektu.

---
+++ TwilioTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+class SMSsender
+{
+    static void Main(string[] args)
+    {
+        //údaje z Twilio účtu
+        string accountSid = "AC50a7eb2f7966b4ab3c95928c7a953c99";
+        string authToken = "62a2bfc39aa6184974c9746e00335a53";
+
+        //odkaz na Twilio
+        TwilioClient.Init(accountSid, authToken);
+        Console.WriteLine("napište zprávu příjemci");
+
+        //vytvoření zprávy
+        var message = MessageResource.Create(
+            body: Console.ReadLine(),
+            from: new Twilio.Types.PhoneNumber("+18316071992"),
+            to: new Twilio.Types.PhoneNumber("+420607148139")
+        );
+
+        Console.WriteLine("Zpráva byla úspěšně odeslána");
+    }
+}
